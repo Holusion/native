95cd72a740e3bc20edcf9ff23264b820145e447d
YannDub <yann59.dubois@gmail.com>
2018-11-22 16:27:49 +0100

    Add ListItem and IconCard in index

--- react-native-holusion/index.js
+++ react-native-holusion/index.js
@@ -1,7 +1,11 @@
 import * as network from 'src/utils/Network'
 import * as assetManager from 'src/utils/AssetManager'
+import ListItem from 'src/components/ListItem'
+import IconCard from 'src/components/IconCard'
 
 export {
   network,
-  assetManager
+  assetManager,
+  ListItem,
+  IconCard
 }
