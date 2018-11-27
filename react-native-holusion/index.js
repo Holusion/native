e234755b82ff08da15dcdd246912bde9683e9d1e
Holusion <holusion@Mustafar.local>
2018-11-27 16:27:17 +0100

    react-native-holusion: add zeroconf manager

--- react-native-holusion/index.js
+++ react-native-holusion/index.js
@@ -1,5 +1,6 @@
 import * as network from './src/utils/Network'
 import * as assetManager from './src/utils/AssetManager'
+import * as zeroconfManager from './src/utils/zeroconfManager'
 import ListItem from './src/components/ListItem'
 import IconCard from './src/components/IconCard'
 import Playlist from './src/components/Playlist'
@@ -7,6 +8,7 @@ import Playlist from './src/components/Playlist'
 export {
   network,
   assetManager,
+  zeroconfManager,
   ListItem,
   IconCard,
   Playlist
---
+++ react-native-holusion/src/utils/zeroconfManager.js
@@ -0,0 +1,29 @@
+import Zeroconf from 'react-native-zeroconf';
+
+const zeroconf = new Zeroconf();
+
+let allProducts = [];
+
+export const manage = (callbackAdd, callbackRemove) => {
+    try {
+        zeroconf.scan('workstation', 'tcp', 'local.');
+        zeroconf.on('resolved', (service) => {
+            let obj = {
+                name: service.name,
+                url: service.addresses
+            }
+            allProducts.push(obj);
+            callbackAdd();
+        });
+        zeroconf.on('remove', (name) => {
+            allProducts = allProducts.filter(elem => elem.name != name);
+            callbackRemove();
+        });
+    } catch(e) {
+    
+    }
+}
+
+export const getUrl = () => {
+    return allProducts[0].url[0];
+}
