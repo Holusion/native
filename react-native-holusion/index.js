994387a2e920c65f7f1d45cb27d2802064394d7e
Holusion <holusion@Mustafar.local>
2018-12-04 12:22:37 +0100

    react-native-holusion: remove zeroconfManager import

--- react-native-holusion/index.js
+++ react-native-holusion/index.js
@@ -1,6 +1,5 @@
 import * as network from './src/utils/Network'
 import * as assetManager from './src/utils/AssetManager'
-import * as zeroconfManager from './src/utils/zeroconfManager'
 import ListItem from './src/components/ListItem'
 import IconCard from './src/components/IconCard'
 import Playlist from './src/components/Playlist'
@@ -8,7 +7,6 @@ import Playlist from './src/components/Playlist'
 export {
   network,
   assetManager,
-  zeroconfManager,
   ListItem,
   IconCard,
   Playlist
