387ef75a77865b7bc162165dbbe6038a6ef555db
Holusion <holusion@Mustafar.local>
2019-04-29 17:57:46 +0200

    Holomouseio_Template: check if params in action functions are valid

--- Holomouseio_Template/src/actions/index.js
+++ Holomouseio_Template/src/actions/index.js
@@ -1,23 +1,17 @@
 export const CHANGE_STATE = 'CHANGE_STATE'
 export const CHANGE_SELECTION_TYPE = 'CHANGE_SELECTION_TYPE'
 
-export const AppState = {
-    INIT: 'INIT',
-    DOWNLOAD_FIREBASE: 'DOWNLOAD_FIREBASE',
-    SEARCH_PRODUCT: 'SEARCH_PRODUCT',
-    READY: 'READY'
-}
-
-export const SelectionType = {
-    ANY_SELECTION: 'ANY_SELECTION',
-    VISITE: 'VISITE',
-    CATALOGUE: 'CATALOGUE'
-}
+export const AppState = {INIT, DOWNLOAD_FIREBASE, SEARCH_PRODUCT, READY}
+export const SelectionType = {ANY_SELECTION, VISITE, CATALOGUE}
 
 export const changeState = (appState) => {
-    return {type: CHANGE_STATE, appState};
+    if(AppState[appState]) {
+        return {type: CHANGE_STATE, appState};
+    }
 }
 
 export const changeSelectionType = (selectionType) => {
-    return {type: CHANGE_SELECTION_TYPE, selectionType};
+    if(SelectionType[selectionType]) {
+        return {type: CHANGE_SELECTION_TYPE, selectionType};
+    }
 }
