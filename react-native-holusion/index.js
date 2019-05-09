82e123dcf3acb6ebd08f7e59aee28a33efb8ee00
Holusion <holusion@Mustafar.local>
2019-05-09 09:52:57 +0200

    react-native-holusion: remove bad import

--- react-native-holusion/index.js
+++ react-native-holusion/index.js
@@ -1,11 +1,9 @@
 import * as network from './src/utils/Network'
 import * as assetManager from './src/utils/AssetManager'
 import Playlist from "./src/components/Playlist";
-import PlaylistItem from "./src/components/PlaylistItem";
 
 export {
   network,
   assetManager,
-  Playlist,
-  PlaylistItem
+  Playlist
 }
