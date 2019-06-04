78a8c9ec4d59aaa3035e53a932836c2b8e58ec81
Holusion <holusion@Mustafar.local>
2019-06-04 16:02:28 +0200

    react-native-holusion: add all oriinal element in playlist items

--- react-native-holusion/src/components/Playlist.js
+++ react-native-holusion/src/components/Playlist.js
@@ -3,18 +3,19 @@ import RNFS from 'react-native-fs';
 import * as network from '../utils/Network';
 
 export const playlistFromContents = (url, contents) => {
-    return contents.map(elem => createPlaylistItem(url, elem.name, elem.title, `file://${RNFS.DocumentDirectoryPath}/${elem.name}.jpg`));
+    return contents.map(elem => createPlaylistItem(elem, url, elem.name, elem.title, `file://${RNFS.DocumentDirectoryPath}/${elem.name}.jpg`));
 }
 
 export const playlistFromNetwork = (url) => {
-    return network.getPlaylist(url).map(elem => createPlaylistItem(url, elem.name, elem.name, `http://${url}:3000/medias/${elem.name}?thumb=true`))
+    return network.getPlaylist(url).map(elem => createPlaylistItem(elem, url, elem.name, elem.name, `http://${url}:3000/medias/${elem.name}?thumb=true`))
 }
 
-const createPlaylistItem = (url, name, title, imageUri) => {
+const createPlaylistItem = (elem, url, name, title, imageUri) => {
     return {
         url,
         name,
         title,
-        imageUri
+        imageUri,
+        ...elem
     }
 }
