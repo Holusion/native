31853b319e2c0f71b811b505fdc4f5766b6b245a
YannDub <yann59.dubois@gmail.com>
2019-08-14 09:57:02 +0200

    StOmer: change size of navigation header

--- StOmer/navigator.js
+++ StOmer/navigator.js
@@ -14,41 +14,43 @@ import {Icon} from 'native-base';
 import { HeaderBackButton } from 'react-navigation';
 
 const wifiIcon = (navigation) => <Icon style={{marginRight: 16, color: navigation.getParam("color", "red")}} name="ios-wifi"/>;
+const headerStyle = {height: 24, display: 'flex'}
+const iconStyle = {top: -8}
 
 export const navigator = {
     setup: {
         id: "Setup",
         screen: SetupScreen,
-        options: navigation => ({headerLeft: null, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
+        options: navigation => ({headerLeft: null, headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
     },
     home: {
         id: "Home",
         screen: HomeScreen,
-        options: navigation => ({headerLeft: null, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
+        options: navigation => ({headerLeft: null, headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
     },
     remerciements: {
         id: "Remerciements",
         screen: RemerciementScreen,
-        options: navigation => ({headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
+        options: navigation => ({headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerLeftContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
     },
     selection: {
         id: "Selection",
         screen: ThemeSelectorScreen,
-        options: navigation => ({headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
+        options: navigation => ({headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerLeftContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
     },
     catalogue: {
         id: "Catalogue",
         screen: CatalogueScreen,
-        options: navigation => ({headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
+        options: navigation => ({headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerLeftContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerBackTitle: strings.back})
     },
     object: {
         id: "Object",
         screen: ObjectScreen,
-        options: navigation => ({headerRight: wifiIcon(navigation), headerLeft: <HeaderBackButton title={strings.back} backTitleVisible={true} onPress={() => navigation.navigate(navigator.catalogue.id)}/>})
+        options: navigation => ({headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerLeftContainerStyle: iconStyle, headerRight: wifiIcon(navigation), headerLeft: <HeaderBackButton title={strings.back} backTitleVisible={true} onPress={() => navigation.navigate(navigator.catalogue.id)}/>})
     },
     objectRemerciements: {
         id: "ObjectRemerciements",
         screen: ObjectRemerciementsScreen,
-        options: navigation => ({headerRight: wifiIcon(navigation)})
+        options: navigation => ({headerRight: wifiIcon(navigation), headerStyle: headerStyle, headerRightContainerStyle: iconStyle, headerLeftContainerStyle: iconStyle, headerBackTitle: strings.back})
     }
 }
