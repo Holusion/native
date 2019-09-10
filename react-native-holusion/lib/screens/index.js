ca1cf8044541a76295c755b556d532aa4af830ea
YannDub <yann59.dubois@gmail.com>
2019-09-10 18:09:20 +0200

    alpha.4

---
+++ react-native-holusion/lib/screens/index.js
@@ -0,0 +1,14 @@
+'use strict';
+import HomeScreen from "./HomeScreen";
+import ConnectScreen from "./ConnectScreen";
+import ObjectScreen from "./ObjectScreen";
+import SynchronizeScreen from "./SynchronizeScreen";
+import UpdateScreen from "./UpdateScreen";
+
+export {
+    HomeScreen,
+    ConnectScreen,
+    ObjectScreen,
+    SynchronizeScreen,
+    UpdateScreen
+}
