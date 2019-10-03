df2ed7b59fa7dbdcfabad8893562dcb02aefce97
YannDub <yann59.dubois@gmail.com>
2019-10-03 09:21:46 +0200

    some preproduction polish on egger

--- Egger/theme.js
+++ Egger/theme.js
@@ -1,6 +1,8 @@
 'use strict';
 import default_vars from "@holusion/react-native-holusion/native-base-theme/variables/platform";
 export const theme = {
+    fontFamily: "MetaPro-Normal",
+    titleFontfamily: "MetaPro-Bold",
     brandPrimary: "#e31937ff",
     brandSecondary: "#666666ff",
     fontSizeH2: 24,
