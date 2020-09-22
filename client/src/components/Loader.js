e7a4c5ae84b393f9717e7f651c210d7aa8b0b042
Sebastien DUMETZ <s.dumetz@holusion.com>
2020-09-22 15:05:28 +0200

    remove some old files

--- client/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loader(){
-  return (<div className="d-flex w-100 justify-content-center pt-4">
-  <div className="spinner-border" style={{width: "3rem", height: "3rem"}} role="status">
-    <span className="sr-only">Loading...</span>
-  </div>
-</div>)
-}
--- client/src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyD4duOI-ueMG6uh2oloB-rvaq-vWtmDxHM",
-    authDomain: "holusion-divers.firebaseapp.com",
-    databaseURL: "https://holusion-divers.firebaseio.com",
-    projectId: "holusion-divers",
-    storageBucket: "holusion-divers.appspot.com",
-    messagingSenderId: "699957095120",
-    appId: "1:699957095120:web:f5bd5fb6af00ab9e0e0cd5"
-};
