2d0a6b293e4280d506a0295ba826cb84c1d7f7d8
Sebastien Dumetz <s.dumetz@holusion.com>
2021-01-06 16:18:49 +0100

    add some tests to sync.js. Fix a bug that would prevent purge from working

--- cache-control/lib/features/sync.js
+++ cache-control/lib/features/sync.js
@@ -11,6 +11,7 @@ import { error, info, warn } from "./logs";
 import { filename } from "../path";
 
 
+/* istanbul ignore next as described in https://github.com/redux-saga/redux-saga/issues/651 */
 export const abortableFetch = (url, opts) => {
   const controller = new AbortController();
   const signal = controller.signal;
@@ -22,6 +23,7 @@ export const abortableFetch = (url, opts) => {
   return promise
 }
 
+/* istanbul ignore next same as abortableFetch, but uses the modified uploadFile method on native platforms */
 export const abortableUpload = (url, file) =>{
   const controller = new AbortController();
   const signal = controller.signal;
@@ -34,6 +36,9 @@ export const abortableUpload = (url, file) =>{
   return promise
 }
 
+export const newSet = (a)=> new Set(a);
+export const getFilenames = (v)=> new Set(Array.from(v).map(v=>filename(v)));
+
 export function* synchronizeProduct(){
   yield put(setSynchronized(false));
   const target = yield select(getActiveProduct);
@@ -42,7 +47,7 @@ export function* synchronizeProduct(){
     return;
   }
 
-  let hasError = false, count= 0;
+  let hasError = false, count= 0, playlist;
 
   const url = `http://${target.url}`;
 
@@ -50,75 +55,85 @@ export function* synchronizeProduct(){
   const items = yield select(getItemsArray);
   const {video, categories=[]} = yield select(getConfig);
   
-  const videos = new Set([
+  const videos = yield call(newSet, [
       ...items.map(i=>i.video),
       video,
       ...categories.map(c=>c.video)
   ].filter(i=>i));
-  
+
   try{
     const res = yield call(abortableFetch, `${url}/playlist`, {
       method: "GET",
     });
-    const playlist = yield call([res, res.json]);
+    playlist = yield call([res, res.json]);
     if(!res.ok){
       let err = new Error(`Failed to fetch playlist (${playlist.message ||res.statusCode})`);
       throw err;
     }else{
-      yield put(info("SYNC", `${playlist.length} vidéos trouvées sur ${target.name}`));
-    }
-    for (let video of videos){
-      const hash = yield select(getHash, video);
-      const name = filename(video);
-      const dist = playlist.find(i=>i.name === name);
-      try{
-        if(dist && dist.conf && dist.conf.hash && dist.conf.hash === hash){
-          continue;
-        }
-        
-        if(dist){
-          yield put(info("SYNC", "Suppression de la vidéo dupliquée "+name));
-          yield call(abortableFetch, `${url}/medias/${name}`, { method: "DELETE" });
-        }
-        yield call(abortableUpload, url, {
-          uri: video,
-          name,
-          hash,
-        })
-        count++;
-      }catch(e){
-        hasError = true;
-        let wrap = new Error(`Failed to upload ${name}`);
-        wrap.context = e.message+ "\n"+ e.stack;
-        yield put(setSynchronized(wrap));
-      }
+      /* istanbul ignore next */
+      const plural = 1 < playlist.length? "s":"";
+      yield put(info("SYNC", `${playlist.length} vidéo${plural} trouvée${plural} sur ${target.name}`));
     }
   }catch(e){
-    hasError = true;
-    let wrap = new Error(`Echec de l'envoi des vidéos au produit`);
+    let wrap = new Error(`Echec de la récupération de la playlist`);
     wrap.context = e.message;
     yield put(setSynchronized(wrap));
+    return; //There is no point in trying to do more
   }
-  if(!hasError && 0< count){
-    yield put(info("SYNC", `${target.name} synchronisé`, `${count} vidéo${1< count?"s":""} synchronisée${1 < count?"s":""} sur ${target.name}`));
+
+  for (let video of videos){
+    const hash = yield select(getHash, video);
+    const name = filename(video);
+    const dist = playlist.find(i=>i.name === name);
+    try{
+      if(dist && dist.conf && dist.conf.hash && dist.conf.hash === hash){
+        continue;
+      }
+      
+      if(dist){
+        yield put(info("SYNC", "Suppression de la vidéo dupliquée "+name));
+        yield call(abortableFetch, `${url}/medias/${name}`, { method: "DELETE" });
+      }
+      yield call(abortableUpload, url, {
+        uri: video,
+        name,
+        hash,
+      })
+      count++;
+    }catch(e){
+      hasError = true;
+      let wrap = new Error(`Failed to upload ${name}`);
+      wrap.context = e.message+ "\n"+ e.stack;
+      yield put(setSynchronized(wrap));
+    }
+  }
+  
+  if(!hasError && 0 < count){
+    /* istanbul ignore next */
+    let plural = 1 < count? "s":"";
+    yield put(info("SYNC", `${target.name} synchronisé`, `${count} vidéo${plural} synchronisée${plural} sur ${target.name}`));
   }else if(!hasError){
     yield put(info("SYNC", `${target.name} synchronisé`, `${target.name} possédait déjà toutes les vidéos`));
   }
 
   if (doPurge === true) {
+    const videoNames = yield call(getFilenames, videos);
     const unused_items = playlist.filter((i) => {
-      return videos.findIndex(v => filename(v) === i.name) === -1;
+      return !videoNames.has(i.name);
     })
     for (let item of unused_items) {
       try {
         yield call(abortableFetch, `${url}/medias/${item.name}`, { method: "DELETE"});
       } catch (e) {
-
+        hasError = true;
         yield put(error("SYNC", `Echec de la suppression de ${item.name}`));
       }
     }
+    /* istanbul ignore next */
     let s = 1 < unused_items.length ? "s" : ""
-    yield put (info("SYNC",`${unused_items.length} ancienne${s} vidéo${s} supprimée${s}`));
+    if(!hasError){
+      yield put (info("SYNC",`${unused_items.length || "aucune"} ancienne${s} vidéo${s} supprimée${s}`));
+    }
   }
   if(!hasError){
     yield put(setSynchronized(true));
--- cache-control/lib/features/sync.test.js
+++ cache-control/lib/features/sync.test.js
@@ -2,10 +2,10 @@
 import {testSaga} from "redux-saga-test-plan";
 import { getConf } from "./conf";
 import { getConfig, getItemsArray } from "./data";
-import { getUncachedFiles } from "./files";
+import { getHash, getUncachedFiles } from "./files";
 import { getActiveProduct } from "./products";
-import { setSynchronized } from "./status";
-import {synchronizeProduct, abortableFetch} from "./sync";
+import { setSynchronized, SET_SYNCHRONIZED } from "./status";
+import {synchronizeProduct, abortableFetch, newSet, getFilenames, abortableUpload} from "./sync";
 
 
 describe("synchronizeProduct", ()=>{
@@ -17,6 +17,7 @@ describe("synchronizeProduct", ()=>{
     .select(getUncachedFiles).next()
     .isDone();
   })
+
   test("abort if files are not synchronized", ()=>{
     testSaga(synchronizeProduct).next()
     .put(setSynchronized(false)).next()
@@ -24,14 +25,33 @@ describe("synchronizeProduct", ()=>{
     .select(getUncachedFiles).next(["foo.png", "bar.mp4"])
     .isDone();
   })
-  test("handle error if playlist can not be fetched", ()=>{
+
+  test("uses the list of required files", ()=>{
+    testSaga(synchronizeProduct).next()
+    .put(setSynchronized(false)).next()
+    .select(getActiveProduct).next({name: "iris32-21", url: "192.168.1.4"})
+    .select(getUncachedFiles).next([])
+    .select(getConf).next({})
+    .select(getItemsArray).next([{id: "foo", video: "file:///applications/app/foo.mp4"}])
+    .select(getConfig).next({
+      video:"file:///applications/app/bar.mp4", 
+      categories:[{video: "file:///applications/app/baz.mp4"}]
+    })
+    .call(newSet, [
+      "file:///applications/app/foo.mp4",
+      "file:///applications/app/bar.mp4",
+      "file:///applications/app/baz.mp4",
+    ]).next(new Set())
+    .finish();
+  })
+
+  test("handle playlist fetch error", ()=>{
     let res = {
       ok: false,
       json: ()=>({message: "Internal Error", code: 500})
     }
-
-    let wrap = new Error(`Echec de l'envoi des vidéos au produit`);
-
+    let wrap = new Error(`Echec de la récupération de la playlist`);
+    
     testSaga(synchronizeProduct).next()
     .put(setSynchronized(false)).next()
     .select(getActiveProduct).next({name: "iris32-21", url: "192.168.1.4"})
@@ -39,34 +59,220 @@ describe("synchronizeProduct", ()=>{
     .select(getConf).next({})
     .select(getItemsArray).next([])
     .select(getConfig).next({})
+    .call(newSet, []).next(new Set())
     .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
     .call([res, res.json]).next(res.json())
     .put(setSynchronized(wrap)).next()
     .isDone();
   })
-  test("handle empty case", ()=>{
-    let res = {
+
+
+
+  describe("upload ", ()=>{
+    let saga, res = {
       ok: true,
       json: ()=>([])
-    }
-    testSaga(synchronizeProduct).next()
-    .put(setSynchronized(false)).next()
-    .select(getActiveProduct).next({name: "iris32-21", url: "192.168.1.4"})
-    .select(getUncachedFiles).next([])
-    .select(getConf).next({})
-    .select(getItemsArray).next([])
-    .select(getConfig).next({})
-    .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
-    .call([res, res.json]).next(res.json())
-    .put({ type: 'LOG_INFO',
-      name: 'SYNC',
-      message: '0 vidéos trouvées sur iris32-21',
-      context: undefined }).next()
-    .put({ type: 'LOG_INFO',
-      name: 'SYNC',
-      message: 'iris32-21 synchronisé',
-      context: 'iris32-21 possédait déjà toutes les vidéos' }).next()
-    .put(setSynchronized(true)).next()
-    .isDone();
+    };
+    beforeEach(()=>{
+      saga = 
+      testSaga(synchronizeProduct).next()
+      .put(setSynchronized(false)).next()
+      .select(getActiveProduct).next({name: "iris32-21", url: "192.168.1.4"})
+      .select(getUncachedFiles).next([])
+      .select(getConf).next({})
+      .select(getItemsArray).next([])
+      .select(getConfig).next({})
+    })
+
+    test("empty case", ()=>{
+      saga
+      .call(newSet, []).next(new Set())
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
+      .call([res, res.json]).next(res.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '0 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: 'iris32-21 synchronisé',
+        context: 'iris32-21 possédait déjà toutes les vidéos' }).next()
+      .put(setSynchronized(true)).next()
+      .isDone();
+    })
+
+    test("on empty product", ()=>{
+      saga
+      .call(newSet, []).next(newSet(["file:///applications/foo/foo.mp4"]))
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
+      .call([res, res.json]).next(res.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '0 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .select(getHash, "file:///applications/foo/foo.mp4").next("xxxxxx")
+      .call(abortableUpload, 'http://192.168.1.4', {
+        uri: "file:///applications/foo/foo.mp4",
+        name: "foo.mp4",
+        hash: "xxxxxx",
+      }).next(res)
+      .put({ 
+        type: 'LOG_INFO',
+        name: 'SYNC',
+        message: 'iris32-21 synchronisé',
+        context: '1 vidéo synchronisée sur iris32-21' 
+      }).next()
+      .put(setSynchronized(true)).next()
+      .isDone();
+    })
+
+    test("replacement", ()=>{
+      let response = {
+        ok: true,
+        json: ()=>([{ name: "foo.mp4", /* no hash */}])
+      }
+      saga
+      .call(newSet, []).next(newSet(["file:///applications/foo/foo.mp4"]))
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(response)
+      .call([response, response.json]).next(response.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '1 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .select(getHash, "file:///applications/foo/foo.mp4").next("xxxxxx")
+      .put({ 
+        type: 'LOG_INFO',
+        name: 'SYNC',
+        message: 'Suppression de la vidéo dupliquée foo.mp4',
+        context: undefined 
+      }).next()
+      .call(abortableFetch, 'http://192.168.1.4/medias/foo.mp4', {method: "DELETE"}).next(res)
+      .call(abortableUpload, 'http://192.168.1.4', {
+        uri: "file:///applications/foo/foo.mp4",
+        name: "foo.mp4",
+        hash: "xxxxxx",
+      }).next(res)
+      .finish();
+    })
+
+    test("skip if up to date", ()=>{
+      let response = {
+        ok: true,
+        json: ()=>([{ name: "foo.mp4", conf: {hash: "xxxxxx"}}])
+      }
+      saga
+      .call(newSet, []).next(newSet(["file:///applications/foo/foo.mp4"]))
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(response)
+      .call([response, response.json]).next(response.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '1 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .select(getHash, "file:///applications/foo/foo.mp4").next("xxxxxx")
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: 'iris32-21 synchronisé',
+        context: 'iris32-21 possédait déjà toutes les vidéos' }).next()
+        .put(setSynchronized(true)).next()
+        .isDone();
+    })
+
+    test("handle failure", ()=>{
+      let videos = ["file:///applications/foo/foo.mp4"];
+      let e = new TypeError("NetworkError when attempting to fetch resource");
+      let wrap = new Error(`Failed to upload foo.mp4`);
+      wrap.context = e.message+ "\n"+ e.stack;
+  
+      saga
+      .call(newSet, []).next(newSet(videos))
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
+      .call([res, res.json]).next(res.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '0 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .select(getHash, "file:///applications/foo/foo.mp4").next("xxxxxx")
+      .call(abortableUpload, 'http://192.168.1.4', {
+        uri: "file:///applications/foo/foo.mp4",
+        name: "foo.mp4",
+        hash: "xxxxxx",
+      }).throw(e)
+      .put({
+        type: SET_SYNCHRONIZED,
+        error: wrap,
+      }).next()
+      .isDone();
+    })
   })
+
+  describe("purge",()=>{
+    let saga, videos;
+    beforeEach(()=>{
+      let res = {
+        ok: true,
+        json: ()=>([{
+          "name": "foo.mp4",
+        }])
+      }
+      videos = new Set();
+      
+      saga = testSaga(synchronizeProduct).next()
+      .put(setSynchronized(false)).next()
+      .select(getActiveProduct).next({name: "iris32-21", url: "192.168.1.4"})
+      .select(getUncachedFiles).next([])
+      .select(getConf).next({purge: true})
+      .select(getItemsArray).next([])
+      .select(getConfig).next({})
+      .call(newSet, []).next(videos)
+      .call(abortableFetch, 'http://192.168.1.4/playlist', { method: 'GET' }).next(res)
+      .call([res, res.json]).next(res.json())
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: '1 vidéo trouvée sur iris32-21',
+        context: undefined }).next()
+      .put({ type: 'LOG_INFO',
+        name: 'SYNC',
+        message: 'iris32-21 synchronisé',
+        context: 'iris32-21 possédait déjà toutes les vidéos' }).next()
+    })
+
+    test("delete unused files", ()=>{
+      saga
+      .call(getFilenames, videos).next(new Set())
+      .call(abortableFetch, `http://192.168.1.4/medias/foo.mp4`, {method: "DELETE"} ).next()
+      .put({
+        type: "LOG_INFO",
+        name: "SYNC",
+        message: '1 ancienne vidéo supprimée',
+        context: undefined,
+      }).next()
+      .put(setSynchronized(true)).next()
+      .isDone();
+    })
+    test("keeps used items", ()=>{
+      saga //Cheat a bit with getFilenames
+      .call(getFilenames, videos).next(new Set(["foo.mp4"]))
+      .put({
+        type: "LOG_INFO",
+        name: "SYNC",
+        message: 'aucune ancienne vidéo supprimée',
+        context: undefined,
+      }).next()
+      .put(setSynchronized(true)).next()
+      .isDone();
+    })
+    test("handle fetch errors", ()=>{
+      saga
+      .call(getFilenames, videos).next(new Set([]))
+      .call(abortableFetch, `http://192.168.1.4/medias/foo.mp4`, {method: "DELETE"} ).throw(new TypeError("NetworkError when attempting to fetch resource"))
+      .put({
+        type: "LOG_ERROR",            
+        name: 'SYNC',
+        message: 'Echec de la suppression de foo.mp4',
+        context: undefined,
+      }).next()
+      .isDone();
+    })
+  })
+
 })
