23d7d4b8c79e14f2d6b4cefa5448864571d9b051
Sebastien DUMETZ <s.dumetz@holusion.com>
2021-11-19 11:23:24 +0100

    Include 2 layers of error boundaries to better handle render errors

--- HolusionCompanion/lib/containers/index.js
+++ HolusionCompanion/lib/containers/index.js
@@ -4,3 +4,4 @@ export {default as ListObjects} from "./ListObjects";
 export {ConnectedController as Controller} from "./Controller";
 export {default as SafeThemeProvider, ThemeProvider} from "./SafeThemeProvider";
 export {RequiredLoadOnceWrapper, RequiredLoadWrapper, ifRequiredLoaded, InitialLoadWrapper} from "./LoadWrapper";
+export {ErrorHandler, withErrorHandler} from "./ErrorHandler";
